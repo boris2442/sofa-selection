0d3e0ea00fb277dc5d2f5464a335df9a7890e1aa
boris2442 <aubinborissimotsebo@gmail.com>
2024-12-12 20:57:42 +0100

    suite javascript

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,7 @@ const header=document.querySelector("nav");
 const btnBurger=document.getElementById("burger-menu");
 const  nav =document.querySelector(".navigation");
 const linkNav=document.querySelectorAll(".navigation a");
+const sections=document.querySelectorAll("section");
 btnBurger.addEventListener("click",()=>{
     nav.classList.toggle("active");
     btnBurger.classList.toggle('bx-x')
@@ -13,3 +14,12 @@ linkNav.forEach(link=>{
     })
 }
 )
+
+window.addEventListener("scrool",()=>{
+    nav.classList.remove("active");
+    btnBurger.classList("bx-x")
+})
+
+window.addEventListener("scroll, ("=>{
+    header.classList.toggle("active")
+})
